4f14a274afcffa4e7ddff9af0d046f039cc13197
cospplredman <62368514+cospplredman@users.noreply.github.com>
2021-01-04 17:35:07 -0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,31 @@
+function drawLine(x1,y1,x2,y2) {
+	screenctx.moveTo(x1+0.5,y1+0.5);
+	screenctx.lineTo(x2+0.5,y2+0.5);
+}
+function drawTextWithOutline(txt,x,y,maxWidth) {
+	screenctx.fillText(txt,x+0.5, y+0.5, maxWidth);
+}
+function fillTriangle(x1, y1, x2, y2, x3, y3) {
+	screenctx.beginPath();
+	screenctx.moveTo(x1+0.5,y1+0.5);
+	screenctx.lineTo(x2+0.5,y2+0.5);
+	screenctx.lineTo(x3+0.5,y3+0.5);
+	screenctx.closePath();
+	screenctx.fill();
+	screenctx.stroke();
+}
+function drawGrid(){
+	screenctx.strokeStyle = "#222222";
+	screenctx.lineWidth=2;
+	screenctx.beginPath();
+	for(let dimPos = 0; dimPos < screenctx.canvas.width + px; dimPos+=px)
+		drawLine(dimPos+(xOff%px),0,dimPos+(xOff%px),screenctx.canvas.height);
+	
+	for(let dimPos = 0;dimPos < screenctx.canvas.height + px; dimPos+=px)
+		drawLine(0,dimPos+(yOff%px),screenctx.canvas.width,dimPos+(yOff%px));
+	
+	screenctx.stroke();
+}
 function lerp(start, end, t) {
 	return (1-t)*start+t*end;
 }
@@ -45,3 +73,4 @@ function paste(x,y,t){
 			nxt.push(alive[i]);
 	alive = nxt;
 }
+
